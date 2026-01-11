75aa4c052d9bdc01ed7f044a1d0bb5f2c08bda89
P_Son <phamtruongsoncva@gmail.com>
2026-01-11 13:09:31 +0700

    Week2_Homework

--- thucHanh/lesson2/main.js
+++ thucHanh/lesson2/main.js
@@ -1,4 +1,23 @@
-import { countOccurences, sumArray, removeDuplicate, flattenArray, isSymmetric, secondLargest
+import { countOccurences, sumArray, removeDuplicate, flattenArray, isSymmetric, secondLargest,
+    sortByProduct, findMostExpensiveProduct, groupByType, isSubset, findMaxKey,
+    mergeObjectsSumValues, countElements,
+    cleanObj,
+    hasDuplicate,
+    sumByGroup,
+    uniqueValues,
+    isPermutation,
+    findLongString,
+    intersection,
+    filteredByMinValue,
+    findLongestValue,
+    groupByFirstLetter,
+    getAdults,
+    convertToArray,
+    sortStringByLength,
+    sumByKey,
+    countWords,
+    findMinKey,
+
 } from './utils.js';
 
 let arr = [1,1,2,3,4,4,5,6]
@@ -11,3 +30,106 @@ let symmetricarr = [1,2,3,4,2,1]
 console.log(isSymmetric(symmetricarr))
 
 console.log(secondLargest(arr))
+
+let products = [
+    { name: 'Product A', price: 30 },
+    { name: 'Product B', price: 20 },
+    { name: 'Product C', price: 50 }
+];
+
+//console.log(sortByProduct(products));
+console.log(findMostExpensiveProduct(products))
+
+let arr2 = [
+    { type: 'fruit', name: 'apple' },
+    { type: 'vegetable', name: 'carrot' },
+    { type: 'fruit', name: 'banana' }
+];
+
+console.log(groupByType(arr2));
+
+let arr1 = [1, 2, 3, 4];
+let arr3 = [2, 4];
+
+console.log(isSubset(arr1, arr3));
+
+let obj = { a: 10, b: 20, c: 15 };
+console.log(findMaxKey(obj))
+
+let obj1 = { a: 10, b: 20 };
+let obj2 = { b: 30, c: 40 };
+
+console.log(mergeObjectsSumValues(obj1, obj2));
+
+let ch_arr =  ['a', 'b', 'a', 'c', 'b', 'a'];
+console.log(countElements(ch_arr));
+
+let clean_obj = { a: 1, b: null, c: undefined, d: 4 }
+console.log(cleanObj(clean_obj));
+
+let duparr = [1, 2, 3, 4, 2]
+console.log(hasDuplicate(duparr))
+
+let grouparr = [
+    { type: 'fruit', price: 10 },
+    { type: 'vegetable', price: 20 },
+    { type: 'fruit', price: 30 }
+];
+
+console.log(sumByGroup(grouparr));
+
+let unique_arr1 = [1, 2, 3];
+let unique_arr2 = [3, 4, 5];
+
+console.log(uniqueValues(unique_arr2, unique_arr1));
+
+let perm_arr1 = [1, 2, 3];
+let perm_arr2 = [3, 2, 1];
+
+console.log(isPermutation(perm_arr1, perm_arr2));
+
+let str_arr = ['abc', 'abcd', 'a']
+console.log(findLongString(str_arr));
+
+let inter_arr1 = [1, 2, 3];
+let inter_arr2 = [2, 3, 4];
+console.log(intersection(inter_arr1, inter_arr2));
+
+let unfiltered = [5, 10, 15, 20];
+let minValue = 12;
+
+console.log(filteredByMinValue(unfiltered, minValue));
+
+let longest_obj = { a: 'cat', b: 'elephant', c: 'dog' };
+console.log(findLongestValue(longest_obj));
+
+let firstCh_arr = ['apple', 'banana', 'avocado', 'blueberry'];
+console.log(groupByFirstLetter(firstCh_arr));
+
+let people = [
+    { name: 'Alice', age: 17 },
+    { name: 'Bob', age: 20 },
+    { name: 'Charlie', age: 16 },
+    { name: 'David', age: 22 }
+];
+console.log(getAdults(people))
+
+let arr_obj = { a: 1, b: 2, c: 3 };
+console.log(convertToArray(arr_obj));
+
+let str_arr_2 = ['a', 'ccc', 'bb'];
+console.log(sortStringByLength(str_arr_2));
+
+let obj_arr = [
+    { type: 'fruit', price: 10 },
+    { type: 'vegetable', price: 20 },
+    { type: 'fruit', price: 30 }
+];
+let key = 'price';
+console.log(sumByKey(obj_arr, key));
+
+let cnt_str = "Hello world! Welcome to JavaScript";
+console.log(countWords(cnt_str));
+
+let minKey_obj = { a: 100, b: 20, c: 50 };
+console.log(findMinKey(minKey_obj));
--- thucHanh/lesson2/utils.js
+++ thucHanh/lesson2/utils.js
@@ -37,7 +37,7 @@ export const flattenArray = (arr) => {
 export const isSymmetric = (arr) => {
     const n = arr.length - 1;
     for (let i = 0; i < (arr.length / 2); i++) {
-        if (arr[i] != arr[n-i]){
+        if (arr[i] != arr[n - i]) {
             return false;
         }
     }
@@ -45,28 +45,282 @@ export const isSymmetric = (arr) => {
 }
 
 export const secondLargest = (arr) => {
-    if (arr.length < 2){
+    if (arr.length < 2) {
         return null;
     }
 
     let mx = -Infinity;
     let sec_mx = -Infinity;
 
-    for(let i=0; i<arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
         const curr = arr[i];
-        if (curr > mx){
+        if (curr > mx) {
             sec_mx = mx;
             mx = curr;
-        }else if (curr > sec_mx && curr < mx) {
+        } else if (curr > sec_mx && curr < mx) {
             sec_mx = curr;
         }
     }
 
-    return sec_mx === -Infinity ? null: sec_mx;
+    return sec_mx === -Infinity ? null : sec_mx;
 };
 
 export const sortByProduct = (product) => {
-    for (let key in product){
-        
+    product.sort((a, b) => a.price - b.price);
+    return product;
+}
+
+export const findMostExpensiveProduct = (products) => {
+    products.sort((a, b) => a.price - b.price);
+    return products[products.length - 1];
+};
+
+export const groupByType = (arr) => {
+    let resObj = {};
+    for (let i = 0; i < arr.length; i++) {
+
+        if (!resObj[arr[i].type]) {
+            resObj[arr[i].type] = [];
+        }
+        resObj[arr[i].type].push(arr[i].name);
     }
-}   
+    return resObj;
+}
+
+export const isSubset = (arr1, arr2) => {
+    if (arr2.length > arr1.length) {
+        return false;
+    }
+    for (let i = 0; i < arr2.length; i++) {
+        if (!arr1.includes(arr2[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+export const findMaxKey = (obj) => {
+    let res = Object.keys(obj)[0]
+    let mx = Object.values(obj)[0];
+    // console.log("res: ", res);
+    // console.log('Mx: ', mx);
+
+    for (const key in obj) {
+        if (obj[key] > mx) {
+            mx = obj[key];
+            res = key;
+        }
+    }
+    return res;
+}
+
+export const mergeObjectsSumValues = (obj1, obj2) => {
+    let resObj = {};
+
+    for (let key in obj1) {
+        if (!resObj[key]) {
+            resObj[key] = obj1[key];
+        } else {
+            resObj[key] += obj1[key];
+        }
+    }
+
+    for (let key in obj2) {
+        if (!resObj[key]) {
+            resObj[key] = obj2[key];
+        } else {
+            resObj[key] += obj2[key];
+        }
+    }
+    return resObj;
+}
+
+export const countElements = (arr) => {
+    let resObj = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (!resObj[arr[i]]) {
+            resObj[arr[i]] = 0;
+        }
+
+        resObj[arr[i]] += 1;
+    }
+    return resObj;
+}
+
+export const cleanObj = (obj) => {
+    for (let key in obj) {
+        if (obj[key] === undefined || obj[key] === null) {
+            delete obj[key];
+        }
+    }
+    return obj;
+}
+
+export const hasDuplicate = (arr) => {
+    let check = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (check.includes(arr[i])) {
+            return true;
+        }
+        check.push(arr[i]);
+    }
+    return false;
+}
+
+export const sumByGroup = (arr) => {
+    let resObj = {};
+    for (let i = 0; i < arr.length; i++) {
+
+        if (!resObj[arr[i].type]) {
+            resObj[arr[i].type] = 0;
+        }
+        resObj[arr[i].type] += (arr[i].price);
+    }
+    return resObj;
+}
+
+export const uniqueValues = (arr1, arr2) => {
+    let groupArr = [...arr1, ...arr2];
+    const res = new Set(groupArr);
+    return res;
+}
+
+export const isPermutation = (arr1, arr2) => {
+    if (arr1.length != arr2.length) {
+        return false;
+    }
+
+    let cnt_obj1 = {};
+    let cnt_obj2 = {};
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (!cnt_obj1[arr1[i]]) {
+            cnt_obj1[arr1[i]] = 0;
+        }
+        cnt_obj1[arr1[i]] += 1;
+    }
+
+    for (let i = 0; i < arr2.length; i++) {
+        if (!cnt_obj2[arr2[i]]) {
+            cnt_obj2[arr2[i]] = 0;
+        }
+        cnt_obj2[arr2[i]] += 1;
+    }
+
+    for (let key in cnt_obj1) {
+        if (cnt_obj1[key] !== cnt_obj2[key]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+export const findLongString = (arr) => {
+    let l = -1;
+    let res = '';
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length > l) {
+            l = arr[i].length;
+            res = arr[i];
+        }
+    }
+    return res;
+}
+
+export const intersection = (arr1, arr2) => {
+    const s = new Set([...arr1, ...arr2]);
+    let res = [];
+    for (let item of s) {
+        if (arr1.includes(item) && arr2.includes(item)) {
+            res.push(item);
+        }
+    }
+    return res;
+}
+
+export const filteredByMinValue = (arr, value) => {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= value) {
+            res.push(arr[i]);
+        }
+    }
+    return res;
+}
+
+
+export const findLongestValue = (obj) => {
+    let arrValue = Object.values(obj);
+    let res = arrValue[0];
+    for (let i = 1; i < arrValue.length; i++) {
+        if (arrValue[i].length > res.length) {
+            res = arrValue[i];
+        }
+    }
+    return res;
+}
+
+export const groupByFirstLetter = (arr) => {
+    let resObj = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (!resObj[arr[i][0]]) {
+            resObj[arr[i][0]] = [];
+        }
+
+        resObj[arr[i][0]].push(arr[i]);
+    }
+    return resObj;
+}
+
+export const getAdults = (people) => {
+    let resArr = [];
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].age >= 18) {
+            resArr.push(people[i]);
+        }
+    }
+    return resArr;
+}
+
+export const convertToArray = (obj) => {
+    let res = [];
+    let keyArr = Object.keys(obj);
+    let valueArr = Object.values(obj);
+
+    for (let i = 0; i < keyArr.length; i++) {
+        res.push([keyArr[i], valueArr[i]]);
+    }
+    return res;
+}
+
+export const sortStringByLength = (arr) => {
+    let res = arr.sort((a, b) => a.length - b.length);
+    return res;
+}
+
+export const sumByKey = (obj, k) => {
+    let summ = 0;
+    for (let i = 0; i < obj.length; i++) {
+        summ += obj[i][k];
+    }
+    return summ;
+}
+
+export const countWords = (str) =>{
+    let words = str.split(' ');
+    return words.length;
+}
+
+export const findMinKey = (obj) =>{
+    let minKey = null;
+    let minVal = Infinity;
+
+    for(const key in obj){
+        if(obj[key] < minVal){
+            minKey = key;
+            minVal = obj[key];
+        }
+    }
+    return minKey;
+}
